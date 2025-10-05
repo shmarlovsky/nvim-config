e4abd40e6f8ecb0675d36852c9831a05a1b681c6
Kiryl Shmarlouski <k.shmarlovsky@gmail.com>
2025-10-05 13:24:01 +0300

    initial based on kickstart.nvim. personal adapted

---
+++ lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,33 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree', silent = true },
+  },
+  opts = {
+    filesystem = {
+      follow_current_file = {
+        enabled = true,
+      },
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+      window = {
+        mappings = {
+          -- ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
