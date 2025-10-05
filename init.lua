eaab29ebd639c1f61191224dc686e18a6b816669
Kiryl Shmarlouski <k.shmarlovsky@gmail.com>
2025-10-05 14:31:34 +0300

    gitsigns: use default signs

--- init.lua
+++ init.lua
@@ -289,15 +289,6 @@ require('lazy').setup({
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
   },
   { 'nvim-tree/nvim-web-devicons' },
 
