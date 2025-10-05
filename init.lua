8d1a601ee26aa6e867019e913da46194a7f35096
Kiryl Shmarlouski <k.shmarlovsky@gmail.com>
2025-10-05 14:28:06 +0300

    add lazygit plugin

--- init.lua
+++ init.lua
@@ -1068,6 +1068,8 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+  require 'custom.plugins.lazygit',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
