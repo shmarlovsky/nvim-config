f5e29d8ad668cd425905f285e4783f71b080ca25
Kiryl Shmarlouski <k.shmarlovsky@gmail.com>
2025-10-14 09:55:41 +0300

    vscode theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,36 +1,14 @@
 -- You can easily change to a different colorscheme.
 
--- tokyonight
---   {-- Change the name of the colorscheme plugin below, and then
---   -- change the command in the config to whatever the name of that colorscheme is.
---   --
---   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
---   'folke/tokyonight.nvim',
---   priority = 1000, -- Make sure to load this before all the other start plugins.
---   config = function()
---     ---@diagnostic disable-next-line: missing-fields
---     require('tokyonight').setup {
---       styles = {
---         comments = { italic = false }, -- Disable italics in comments
---       },
---     }
---
---     -- Load the colorscheme here.
---     -- Like many other themes, this one has different styles, and you could load
---     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
---     vim.cmd.colorscheme 'tokyonight-night'
---   end,
--- },
-
 return {
-  'catppuccin/nvim',
+  'Mofiqul/vscode.nvim',
 
   priority = 1000, -- Make sure to load this before all the other start plugins.
   lazy = false,
   config = function()
     ---@diagnostic disable-next-line: missing-fields
-    require('catppuccin').setup {
-      flavour = 'mocha', -- latte, frappe, macchiato, mocha
+    require('vscode').setup {
+      -- flavour = 'mocha', -- latte, frappe, macchiato, mocha
       -- Alternatively set style in setup
       -- style = 'light'
 
@@ -38,13 +16,40 @@ return {
       -- transparent = true,
 
       -- Enable italic comment
-      italic_comments = true,
+      -- italic_comments = true,
 
       -- Enable italic inlay type hints
-      italic_inlayhints = true,
+      -- italic_inlayhints = true,
     }
 
     -- Load the colorscheme here.
-    vim.cmd.colorscheme 'catppuccin'
+    vim.cmd.colorscheme 'vscode'
   end,
 }
+
+-- return {
+--   'catppuccin/nvim',
+--
+--   priority = 1000, -- Make sure to load this before all the other start plugins.
+--   lazy = false,
+--   config = function()
+--     ---@diagnostic disable-next-line: missing-fields
+--     require('catppuccin').setup {
+--       flavour = 'mocha', -- latte, frappe, macchiato, mocha
+--       -- Alternatively set style in setup
+--       -- style = 'light'
+--
+--       -- Enable transparent background
+--       -- transparent = true,
+--
+--       -- Enable italic comment
+--       italic_comments = true,
+--
+--       -- Enable italic inlay type hints
+--       italic_inlayhints = true,
+--     }
+--
+--     -- Load the colorscheme here.
+--     vim.cmd.colorscheme 'catppuccin'
+--   end,
+-- }
