db51a4c7d6769435a327b6c155a9e80e4b9a7e95
Kiryl Shmarlouski <kiryl.shmarlouski@ext.fmc-ag.com>
2026-06-13 20:37:15 +0300

    language servers and some other setup

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -172,6 +172,12 @@ return {
     vim.lsp.enable 'ruff'
 
     vim.lsp.config('pyright', {
+      cmd = { 'pyright-langserver', '-p', 'pyrightconfig.json', '--stdio' },
+      root_markers = {
+        -- 'pyproject.toml',
+        'pyrightconfig.json',
+        '.git',
+      },
       settings = {
         pyright = {
           -- Using Ruff's import organizer
@@ -187,100 +193,100 @@ return {
       },
     })
     vim.lsp.enable 'pyright'
-    -- -- Enable the following language servers
-    -- --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    -- --
-    -- --  Add any additional override configuration in the following tables. Available keys are:
-    -- --  - cmd (table): Override the default command used to start the server
-    -- --  - filetypes (table): Override the default list of associated filetypes for the server
-    -- --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    -- --  - settings (table): Override the default settings passed when initializing the server.
-    -- --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-    -- local servers = {
-    --   -- clangd = {},
-    --   -- gopls = {},
-    --   pyright = {
-    --     settings = {
-    --       pyright = {
-    --         -- Using Ruff's import organizer
-    --         disableOrganizeImports = true,
-    --       },
-    --       python = {
-    --         analysis = {
-    --           -- Ignore all files for analysis to exclusively use Ruff for linting
-    --           ignore = { '*' },
-    --         },
-    --       },
-    --     },
-    --   },
-    --   ruff = {},
-    --   -- rust_analyzer = {},
-    --   -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-    --   --
-    --   -- Some languages (like typescript) have entire language plugins that can be useful:
-    --   --    https://github.com/pmizio/typescript-tools.nvim
-    --   --
-    --   -- But for many setups, the LSP (`ts_ls`) will work just fine
-    --   -- ts_ls = {},
-    --   --
+    -- Enable the following language servers
+    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
     --
-    --   lua_ls = {
-    --     -- cmd = { ... },
-    --     -- filetypes = { ... },
-    --     -- capabilities = {},
-    --     settings = {
-    --       Lua = {
-    --         completion = {
-    --           callSnippet = 'Replace',
-    --         },
-    --         -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-    --         -- diagnostics = { disable = { 'missing-fields' } },
-    --       },
-    --     },
-    --   },
-    -- }
+    --  Add any additional override configuration in the following tables. Available keys are:
+    --  - cmd (table): Override the default command used to start the server
+    --  - filetypes (table): Override the default list of associated filetypes for the server
+    --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+    --  - settings (table): Override the default settings passed when initializing the server.
+    --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+    local servers = {
+      -- clangd = {},
+      -- gopls = {},
+      pyright = {
+        settings = {
+          pyright = {
+            -- Using Ruff's import organizer
+            disableOrganizeImports = true,
+          },
+          python = {
+            analysis = {
+              -- Ignore all files for analysis to exclusively use Ruff for linting
+              ignore = { '*' },
+            },
+          },
+        },
+      },
+      ruff = {},
+      -- rust_analyzer = {},
+      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+      --
+      -- Some languages (like typescript) have entire language plugins that can be useful:
+      --    https://github.com/pmizio/typescript-tools.nvim
+      --
+      -- But for many setups, the LSP (`ts_ls`) will work just fine
+      -- ts_ls = {},
+      --
+
+      lua_ls = {
+        -- cmd = { ... },
+        -- filetypes = { ... },
+        -- capabilities = {},
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = 'Replace',
+            },
+            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            -- diagnostics = { disable = { 'missing-fields' } },
+          },
+        },
+      },
+    }
+
+    -- Ensure the servers and tools above are installed
     --
-    -- -- Ensure the servers and tools above are installed
-    -- --
-    -- -- To check the current status of installed tools and/or manually install
-    -- -- other tools, you can run
-    -- --    :Mason
-    -- --
-    -- -- You can press `g?` for help in this menu.
-    -- --
-    -- -- `mason` had to be setup earlier: to configure its options see the
-    -- -- `dependencies` table for `nvim-lspconfig` above.
-    -- --
-    -- -- You can add other tools here that you want Mason to install
-    -- -- for you, so that they are available from within Neovim.
-    -- local ensure_installed = vim.tbl_keys(servers or {})
-    -- vim.list_extend(ensure_installed, {
-    --   'stylua', -- Used to format Lua code
-    -- })
-    -- require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+    -- To check the current status of installed tools and/or manually install
+    -- other tools, you can run
+    --    :Mason
     --
-    -- require('mason-lspconfig').setup {
-    --   ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-    --   automatic_installation = false,
-    --   automatic_enable = {
-    --     exclude = {
-    --       'ruff',
-    --       'pyright',
-    --     },
-    --   },
-    --   handlers = {
-    --     function(server_name)
-    --       print 'CONFIGURING LSP'
-    --       print(server_name)
-    --       local server = servers[server_name] or {}
-    --       -- This handles overriding only values explicitly passed
-    --       -- by the server configuration above. Useful when disabling
-    --       -- certain features of an LSP (for example, turning off formatting for ts_ls)
-    --       server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-    --       require('lspconfig')[server_name].setup(server)
-    --       vim.lsp.enable(server_name)
-    --     end,
-    --   },
-    -- }
+    -- You can press `g?` for help in this menu.
+    --
+    -- `mason` had to be setup earlier: to configure its options see the
+    -- `dependencies` table for `nvim-lspconfig` above.
+    --
+    -- You can add other tools here that you want Mason to install
+    -- for you, so that they are available from within Neovim.
+    local ensure_installed = vim.tbl_keys(servers or {})
+    vim.list_extend(ensure_installed, {
+      'stylua', -- Used to format Lua code
+    })
+    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+    require('mason-lspconfig').setup {
+      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+      automatic_installation = true,
+      automatic_enable = {
+        exclude = {
+          'ruff',
+          'pyright',
+        },
+      },
+      handlers = {
+        function(server_name)
+          -- print 'CONFIGURING LSP'
+          -- print(server_name)
+          local server = servers[server_name] or {}
+          -- This handles overriding only values explicitly passed
+          -- by the server configuration above. Useful when disabling
+          -- certain features of an LSP (for example, turning off formatting for ts_ls)
+          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          require('lspconfig')[server_name].setup(server)
+          vim.lsp.enable(server_name)
+        end,
+      },
+    }
   end,
 }
